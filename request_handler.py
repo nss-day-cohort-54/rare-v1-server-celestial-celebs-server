79a9b892ae740dc48a84722049684de12a2a8560
Kevin Caruso <Kjcaruso12@gmail.com>
2022-04-21 11:35:19 -0500

    added create_category function and updated Do_POST in request_handler

--- request_handler.py
+++ request_handler.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 from views import get_all_categories
 from views import get_single_post, get_all_posts
+from views.categories_requests import create_category
 
 from views.user import create_user, login_user
 
@@ -103,6 +104,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             response = login_user(post_body)
         if resource == 'register':
             response = create_user(post_body)
+        if resource == 'categories':
+            response = create_category(post_body)
 
         self.wfile.write(response.encode())
 
